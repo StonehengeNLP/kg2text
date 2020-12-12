efdca303946cd4d414c692e227270ed67c3117cb
tongplw <bangkok.tong@gmail.com>
2020-12-12 22:21:47 +0700

    updated as setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
+# install OpenNMT
+wget https://github.com/OpenNMT/OpenNMT-py/archive/0.9.0.tar.gz
+tar -xf 0.9.0.tar.gz
+cd OpenNMT-py-0.9.0
+ls
+pip3 install -e . 
+cd ..
+rm 0.9.0.tar.gz
+rm -r OpenNMT-py-0.9.0
+
+# setup data
 git clone https://github.com/rikdz/GraphWriter.git
 mv GraphWriter/data data
 rm -r GraphWriter
@@ -7,4 +18,5 @@ mv unprocessed.train.json unprocessed.training.json
 mv unprocessed.val.json unprocessed.dev.json
 cd ..
 
+# download model
 wget https://public.ukp.informatik.tu-darmstadt.de/ribeiro/graph2text/model_agenda_cge_lw.pt
